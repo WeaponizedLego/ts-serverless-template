e8c66bc9d2b0197cf29ece2f15d1af4948aeb3c6
Glen <glen@soop.dk>
2022-03-03 17:07:22 +0100

    dotfiles

---
+++ .eslintrc.js
@@ -0,0 +1,63 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'airbnb-base',
+    'plugin:prettier/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'prettier'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: ['@typescript-eslint', 'prettier', 'import'],
+  rules: {
+    'prefer-const': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    'prettier/prettier': 'error',
+    'import/extensions': 'off',
+    'import/no-unresolved': 'error',
+    'no-console': 'off',
+    'import/prefer-default-export': 'off',
+    // 'no-use-before-declare': ['error', { variables: false, functions: false }],
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['off'],
+    'import/order': [
+      'error',
+      {
+        'newlines-between': 'never',
+        groups: [
+          ['builtin', 'external'],
+          ['internal', 'parent', 'sibling', 'index']
+        ]
+      }
+    ],
+    indent: ['error', 2, { SwitchCase: 1 }],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never']
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts']
+    },
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true, // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
+
+        // Choose from one of the "project" configs below or omit to use <root>/tsconfig.json by default
+
+        // use <root>/path/to/folder/tsconfig.json
+        project: './tsconfig.json'
+      }
+    }
+  }
+}
