e8b454196adaf2b6831b6460657519234ac4e9d7
Glen <glen@soop.dk>
2022-03-04 14:46:21 +0100

    .

--- scripts/runtimeLayer.sh
+++ scripts/runtimeLayer.sh
@@ -1,34 +1,40 @@
 #!/bin/bash
 function prepare_node_modules_lambda_layer() {
-  echo "Cleaning up workspace ..."
-  rm -rf node_modules
-  rm -rf ./layers/layerRuntime.zip
-
-  echo "Installing node modules -- production ..."
+  echo "Preparing node_modules lambda layer..."
+  cd layers/layerRuntime/nodejs || exit
+  ls -a
   yarn install
-
-  echo "Creating layer ..."
-  mkdir -p layers/layerRuntime
-  mkdir -p layers/layerRuntime/nodejs
-
-  echo "Prepare server node_modules lambda layer ..."
-  cp -r node_modules layers/layerRuntime/nodejs
-
-  echo "Compressing ..."
-  #pushd layers/layerRuntime && tar -zcf /tmp/layerRuntime.tar.gz . && mv /tmp/layerRuntime.tar.gz ./layerRuntime.tar.gz
-  pushd layers/layerRuntime && zip -r /tmp/layerRuntime * -x .* -x "package.json" -x "*.log" -x *.md -1 -q && mv /tmp/layerRuntime.zip ../layerRuntime.zip
-
-  echo "Remove unzipped files and folders ..."
-  rm -rf ./layers/layerRuntime
-
-  echo "Stats:"
-  ls -lh ./layers/layerRuntime.zip
-
-  echo "Installing node modules -- development ..."
-  yarn install
-
-  popd
-
-  echo "Done."
+  cd ../../../
+  ls -a
 }
+#  echo "Cleaning up workspace ..."
+#  rm -rf node_modules
+#  rm -rf ./layers/layerRuntime.zip
+#
+#  echo "Installing node modules -- production ..."
+#  yarn install
+#
+#  echo "Creating layer ..."
+#  mkdir -p layers/layerRuntime
+#  mkdir -p layers/layerRuntime/nodejs
+#
+#  echo "Prepare server node_modules lambda layer ..."
+#  cp -r node_modules layers/layerRuntime/nodejs
+#
+#  echo "Compressing ..."
+#  #pushd layers/layerRuntime && tar -zcf /tmp/layerRuntime.tar.gz . && mv /tmp/layerRuntime.tar.gz ./layerRuntime.tar.gz
+#  pushd layers/layerRuntime && zip -r /tmp/layerRuntime * -x .* -x "package.json" -x "*.log" -x *.md -1 -q && mv /tmp/layerRuntime.zip ../layerRuntime.zip
+#
+#  echo "Remove unzipped files and folders ..."
+#  rm -rf ./layers/layerRuntime
+#
+#  echo "Stats:"
+#  ls -lh ./layers/layerRuntime.zip
+#
+#  echo "Installing node modules -- development ..."
+#  yarn install
+#
+#  popd
+#
+#  echo "Done."
 prepare_node_modules_lambda_layer
